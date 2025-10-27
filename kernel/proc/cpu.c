ce06ecaae3d39091e8bdf9117c3a1b38f7e1b7a3
hbh <hbhkl19@qq.com>
2025-10-27 16:32:56 +0800

    write functions in cpu.c

--- kernel/proc/cpu.c
+++ kernel/proc/cpu.c
@@ -5,15 +5,21 @@ static cpu_t cpus[NCPU];
 
 cpu_t* mycpu(void)
 {
-
+  int id = r_tp();
+  return &cpus[id];
 }
 
 int mycpuid(void) 
 {
-
+  int id = r_tp();
+  return id;
 }
 
 proc_t* myproc(void)
 {
-    
+    push_off();
+    cpu_t* c = mycpu();    
+    proc_t* p = c->proc;   
+    pop_off();             
+    return p;
 }
